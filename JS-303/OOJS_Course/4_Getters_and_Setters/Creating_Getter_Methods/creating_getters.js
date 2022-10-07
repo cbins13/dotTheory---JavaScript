1512e59f76ef13d6dfc38387db44427329c4bb33
Christian Binay-an <christianbinayan3@gmail.com>
2022-10-07 17:32:51 +0800

    JS-264: Update getter method for creating_getters.js file

--- JS-303/OOJS_Course/4_Getters_and_Setters/Creating_Getter_Methods/creating_getters.js
+++ JS-303/OOJS_Course/4_Getters_and_Setters/Creating_Getter_Methods/creating_getters.js
@@ -1,24 +1,27 @@
 class Student {
-    constructor(gpa, credits){
-        this.gpa = gpa;
-        this.credits = credits;
-    }
+  constructor(gpa, credits) {
+    this.gpa = gpa;
+    this.credits = credits;
+  }
 
-    stringGPA() {
-        return this.gpa.toString();
-    }
+  stringGPA() {
+    return this.gpa.toString();
+  }
 
-    get level(){
-        if(this.credits > 90){
-            return 'Senior';
-        } else if(this.credits <= 90 && this.credits >= 61){
-            return 'Junior';
-        } else if(this.credits <= 60 && this.credits >= 31){
-            return 'Sophomore';        
-        } else if(this.credits <= 30){
-            return 'Freshman';
-        }
+  get level() {
+    const studCreds = this.credits;
+
+    switch (true) {
+      case studCreds > 90:
+        return "Senior";
+      case studCreds <= 90 && studCreds >= 61:
+        return "Junior";
+      case studCreds <= 60 && studCreds >= 31:
+        return "Sophomore";
+      case studCreds <= 30:
+        return "Freshman";
     }
+  }
 }
 
 const student = new Student(3.9);
